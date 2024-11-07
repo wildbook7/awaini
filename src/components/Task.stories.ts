885cb5d5cfe0d61aa45f4c6b4f9395261ed40025
wildbook7 <wildbook7@gmail.com>
2024-11-07 14:49:21 +0900

    Storybook - Task のストーリーを追加

--- src/components/Task.stories.ts
+++ src/components/Task.stories.ts
@@ -3,34 +3,33 @@ import Task from "./Task";
 export default {
   component: Task,
   title: "Task",
+  tags: ["autodocs"],
 };
-
-// const Template = (args: any) => <Task {...args} />;
 
 export const Default = {
   args: {
     task: {
       id: "1",
       title: "Test Task",
-      state: "TASK_PINNED",
-      updatedAt: new Date(2021, 0, 1, 9, 0),
+      state: "TASK_INBOX",
     },
   },
 };
-// Template.bind({});
 
-// export const Pinned = Template.bind({});
-// Pinned.args = {
-//   task: {
-//     ...Default.args.task,
-//     state: "TASK_PINNED",
-//   },
-// };
+export const Pinned = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: "TASK_PINNED",
+    },
+  },
+};
 
-// export const Archived = Template.bind({});
-// Archived.args = {
-//   task: {
-//     ...Default.args.task,
-//     state: "TASK_ARCHIVED",
-//   },
-// };
+export const Archived = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: "TASK_ARCHIVED",
+    },
+  },
+};
